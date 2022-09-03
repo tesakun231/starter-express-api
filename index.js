3e88a28797e7d834b59800a2b63d04bd649d7ada
tesakun231 <112684077+tesakun231@users.noreply.github.com>
2022-09-03 16:34:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
-const { spawn } = require("child_process");
-
-const ls = spawn("ls", ["-la"]);
-
-ls.stdout.on("data", data => {
-    console.log(`stdout: ${data}`);
-});
-
-ls.stderr.on("data", data => {
-    console.log(`stderr: ${data}`);
-});
-
-ls.on('error', (error) => {
-    console.log(`error: ${error.message}`);
-});
-
-ls.on("close", code => {
-    console.log(`child process exited with code ${code}`);
-});
+const express = require('express')
+const app = express()
+app.all('/', (req, res) => {
+    console.log("Just got a request!")
+    res.send('Yo!')
+})
+app.listen(process.env.PORT || 3000)
